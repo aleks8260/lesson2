f485ba7ec844339a809fb1749d5ed532a9029306
Web <web@it.step>
2018-09-24 20:04:39 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+    //echo "Hello world!"; //вывод текста
+    //Переменные
+    //$var != $vaR;
+    //$var_FR67
+    //$str = "lorem";
+    //$str = 12;
+    //нельзя объявлять переменные: str = "", 45str;
+    //echo $str;
+    //echo PHP_INT_MAX; //integer константа max
+    //Array
+    //$array = [];
+    //$array = Array();//php 5.5
+/*    $arr = ["orange", "red", "white"]; //простой массив
+    echo $arr[0]; //orange
+    $assoc = [
+        'color' => 'red',
+        'age' => 25,
+        'nickname' => 'Max'
+    ];
+    class A[]
+    $obj = new A;
+    $obj->color = 'red';
+
+    //echo $assoc['color']; //red
+    echo $obj->color; //red
+    TRUE == 1; //true
+    FALSE == 0; //false
+    "" == FALSE; // пустая строка
+    " " == TRUE; //не пустая строка
+    $var = true;
+    echo $var++; //не действует инкремент
+    $var = NULL; //пустое значение*/
+    /*Проверка на существование переменной*/
+/*    $var =["orange","red","white"];
+    unset($var); //удаление переменной
+    unset($var['1']); //удаление значения массива
+    print_r($var); //Отладка массива
+    echo count($var).PHP_EOL; //длинна массива PHP_EOL - пробел
+
+    if(isset($var)){
+        echo "ok";
+    }else{
+        echo "not";
+    }*/
+/*    $var = 45;
+    echo is_int($var); //возвращает тру 1, либо фелс ничего
+    echo gettype($var); //проверка типа данных
+    $var = (bool)0;
+    var_dump($var);*/
+/*
+    $var = 90;
+    $var1 = 90;
+    echo "$var + $var1"; //покажет значения
+    echo '$var + $var1'; //покажет переменные*/
+
+?>
